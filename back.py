aa92e4a6b91ee61e0d3bde7717b8cd21131daf91
Leoorchid <leoorchid10@gmail.com>
2026-01-01 06:51:58 -0500

    Finished mvp Backend
    Added core Account features and also Cardset

--- back.py
+++ back.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 import sqlite3
 from pydantic import BaseModel
 import bcrypt
+from typing import List, Dict
 
 app = FastAPI()
 
@@ -39,9 +40,52 @@ def signAcc(username, password):
         return {"message": "Fail"}
 
 
+def delAcc(username):
+    conn = sqlite3.connect("mem.db")
+    cur = conn.cursor()
+    cur.execute("DELETE FROM users WHERE username = ?", (username,))
+    conn.commit()
+    conn.close()
+
+
+def delSet(name):
+    conn = sqlite3.connect("cards.db")
+    cur = conn.cursor()
+    cur.execute(f"DROP TABLE IF EXISTS {name}")
+    conn.commit()
+    conn.close()
+
+
+def changeSet(num, term, definition, name, newName):
+    conn = sqlite3.connect("cards.db")
+    cur = conn.cursor()
+    cur.execute(f"UPDATE {name} SET term = ? WHERE id=?", (term, num))
+    cur.execute(f"UPDATE {name} SET definition = ? WHERE id=?", (definition, num))
+    cur.execute(f"ALTER TABLE {name} RENAME TO {newName}")
+    conn.commit()
+    conn.close()
+
+
 class Acc(BaseModel):
     username: str
     password: str
+
+
+class Card(BaseModel):
+    num: int
+    term: str
+    definition: str
+
+
+class CardSet(BaseModel):
+    title: str
+    cards: List[Card]
+
+
+class CardSetC(BaseModel):
+    title: str
+    newTitle: str
+    cards: List[Card]
 
 
 @app.get("/")
@@ -59,4 +103,44 @@ def pcreateAcc(acc: Acc):
     return createAcc(acc.username, acc.password)
 
 
-signAcc("Guest", "123456")
+@app.post("/post/deleteAcc")
+def pdelAcc(acc: Acc):
+    return delAcc(acc.username)
+
+
+@app.post("/post/createSet")
+def createSet(cardset: CardSet):
+    conn = sqlite3.connect("cards.db")
+    cur = conn.cursor()
+    cur.execute(
+        f"CREATE TABLE IF NOT EXISTS {cardset.title} (id INTEGER PRIMARY KEY , term TEXT UNIQUE NOT NULL, definition TEXT NOT NULL);"
+    )
+    conn.commit()
+    conn.close
+    cur = conn.cursor()
+    try:
+        for card in cardset.cards:
+            cur.execute(
+                f"INSERT INTO {cardset.title} (id,term,definition) VALUES(?,?,?)",
+                (card.num, card.term, card.definition),
+            )
+        conn.commit()
+        conn.close()
+    except ():
+        print("Failed")
+        return
+    else:
+        print("Saved")
+    return
+
+
+@app.post("/post/delSet")
+def pdelSet(cardSet: CardSet):
+    delSet(cardSet.title)
+
+
+@app.post("/post/changeSet")
+def pchangeSet(cardSet: CardSetC):
+    for card in cardSet.cards:
+        print(card.num, card.term, card.definition, cardSet.title, cardSet.newTitle)
+        changeSet(card.num, card.term, card.definition, cardSet.title, cardSet.newTitle)
