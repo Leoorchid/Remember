8147fda9362c6d792d41794bbc0c331ec9e18963
Leoorchid <leoorchid10@gmail.com>
2026-01-12 10:14:11 -0500

    Test is js done

--- front/home.js
+++ front/home.js
@@ -6,3 +6,13 @@ viewBtn = document.getElementById("viewBtn")
 createBtn.addEventListener("click", () => {
     window.location.href = "create.html"
 })
+
+arr = ["a","b","c","d","e"]
+
+arr.forEach(l => {
+    rng = Math.ceil(Math.random()*5)
+    console.log(rng)
+     a= arr.pop(rng)
+     console.log(a)
+    
+});
--- front/view.js
+++ front/view.js
@@ -62,16 +62,136 @@ function viewCard(data, title) {
         cardContainer.append(cardDiv)
 
 
-        testViewBtn.addEventListener("click", () => {
-            cardContainer.style.display = "none"
-            //DO NEXT randomize test sheet
-
-        })
+        
 
 
     }
 
+    testViewBtn.addEventListener("click", () => {
+        cardContainer.style.display = "none"
+        let arrTerms = []
+        let arrDefs = []
+        //DO NEXT randomize test sheet
+        i =1
+        for(const key in data){
+            
+            arrTerms.push(key)
+            arrDefs.push(data[key])
+            
+        }
+        
+        
 
+
+
+        //-----------------
+
+        const testSheet = document.createElement("div")
+        document.body.append(testSheet)
+
+        arrTerms.forEach(term => {
+            let defs = [data[term]]
+            let copyDefs = arrDefs.slice()
+            
+            let questionDiv = document.createElement("div")
+
+
+            let question = document.createElement("div")
+            question.innerText = term
+
+            field = document.createElement("fieldset")
+            let a1D = document.createElement("div")
+            let a1 = document.createElement("input")
+            a1.type = "radio"
+            let a1L = document.createElement("label")
+            
+
+            a1D.append(a1)
+            a1D.append(a1L)
+
+            let a2D = document.createElement("div")
+            let a2 = document.createElement("input")
+            a2.type = "radio"
+            let a2L = document.createElement("label")
+
+            
+
+            a2D.append(a2)
+            a2D.append(a2L)
+
+            let a3D = document.createElement("div")
+            let a3 = document.createElement("input")
+            a3.type = "radio"
+            let a3L = document.createElement("label")
+
+           
+
+            a3D.append(a3)
+            a3D.append(a3L)
+            let a4D = document.createElement("div")
+            let a4 = document.createElement("input")
+            a4.type = "radio"
+            let a4L = document.createElement("label")
+
+            
+            
+            
+            
+            let rng 
+            while(true){
+                rng = copyDefs.splice(Math.ceil(Math.random()*copyDefs.length)-1,1)
+                if(rng!=data[term]){
+                    defs.push(rng[0])
+                    break
+                } 
+            }
+            while(true){
+                rng = copyDefs.splice(Math.ceil(Math.random()*copyDefs.length)-1,1)
+                if(rng!=data[term]){
+                    defs.push(rng[0])
+                    break
+                }   
+            }
+            while(true){
+                rng = copyDefs.splice(Math.ceil(Math.random()*copyDefs.length)-1,1)
+                if(rng!=data[term]){
+                    defs.push(rng[0])
+                    break
+                }
+            }
+
+            a1L.innerText = defs.pop(Math.ceil(Math.random()*defs.length-1))
+            a2L.innerText = defs.pop(Math.ceil(Math.random()*defs.length-1))
+            a3L.innerText = defs.pop(Math.ceil(Math.random()*defs.length-1))
+            a4L.innerText = defs.pop(Math.ceil(Math.random()*defs.length-1))
+            
+            
+            
+            
+
+            console.log(defs)
+                
+                
+                
+            
+
+            a4D.append(a4)
+            a4D.append(a4L)
+
+            questionDiv.append(question)
+            field.append(a1D)
+            field.append(a2D)
+            field.append(a3D)
+            field.append(a4D)
+            questionDiv.append(field)
+
+            document.body.append(questionDiv)
+            
+
+        });
+
+
+    })
 
 
 }
